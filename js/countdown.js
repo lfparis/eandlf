98f4e87a303548035ad7eed0905a3c846d748dde
lfparis <lfparis@gmail.com>
2017-02-18 18:59:24 -0500

    6

---
+++ js/countdown.js
@@ -0,0 +1,10 @@
+function countdown() {
+  var wedding = new Date(2017, 8, 24);
+  var difference = wedding.getTime() - Date.now();
+  return Math.floor(difference / 1000 / 60 / 60 / 24);
+}
+
+$(document).ready(function() {
+  /*$('#dateTime').text(countdown());*/
+  $('#dateTime').text(countdown() + " DAYS TO GO!");
+});
