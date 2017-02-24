e346fdb29b23ad2264c5cd07e79d2653b5d9495c
lfparis <lfparis@gmail.com>
2017-02-24 08:55:56 -0500

    coutndown temporary fix

--- js/countdown.js
+++ js/countdown.js
@@ -1,11 +1,10 @@
 function countdown() {
-  var wedding = new Date(2017, 7, 24);
+  var wedding = new Date(2017, 7, 25);
   var today = new Date();
   var difference = wedding.getTime() - today.getTime();
-  return Math.floor(difference / 1000 / 60 / 60 / 24)+1;
+  return Math.floor(difference / 1000 / 60 / 60 / 24);
 }
 
 $(document).ready(function() {
-  /*$('#dateTime').text(countdown());*/
   $('#dateTime').text(countdown() + " DAYS TO GO!");
 });
