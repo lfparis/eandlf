a2d35779ada7d4caa40f579018bee527fc0ec67f
lfparis <lfparis@gmail.com>
2017-02-24 08:48:46 -0500

    coundtown fix 2

--- js/countdown.js
+++ js/countdown.js
@@ -1,8 +1,8 @@
 function countdown() {
   var wedding = new Date(2017, 8, 24);
   var today = new Date();
-  var difference = today.getTime() - wedding.getTime();
-  return Math.Floor(difference / 1000 / 60 / 60 / 24);
+  var difference = wedding.getTime() - today.getTime();
+  return Math.floor(difference / 1000 / 60 / 60 / 24);
 }
 
 $(document).ready(function() {
